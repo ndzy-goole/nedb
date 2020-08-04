4455ede9a0da157a051896c898f2a3398e2c188b
张一 <2326962137@qq.com>
2020-08-04 13:18:29 +0800

    2020-08-04-13-18-20

---
+++ db.js
@@ -0,0 +1,34 @@
+// 加载模块
+const path = require('path');
+const nedb = require('nedb');
+
+const data = path.join(process.cwd(), 'data.db');
+
+// 实例化连接对象（不带参数默认为内存数据库）
+const db = new nedb({
+  filename: data,
+  autoload: true
+});
+// 插入单项
+db.insert(
+  {
+    name: 'tom'
+  },
+  (err, ret) => {}
+);
+
+// 插入多项
+db.insert([{ name: 'tom' }, { name: 'jerry' }], (err, ret) => {});
+
+// 查询单项
+db.findOne(
+  {
+    name: 'tom'
+  },
+  (err, ret) => {
+    // console.log(ret);
+  }
+);
+db.find({}, (err, ret) => {
+  console.log(ret);
+});
